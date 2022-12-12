1440834df72092f614d7aa37029f1b87b22feee6
mitsu1119 <hachimitsu1119@gmail.com>
2022-12-13 00:15:12 +0900

    [add] Integer: 比較演算子

--- Integer.py
+++ Integer.py
@@ -8,24 +8,34 @@ class Integer:
 
     def __add__(self, other):
         return Integer(self.value + other.value)
-
     def __sub__(self, other):
         return Integer(self.value - other.value)
-
     def __mul__(self, other):
         return Integer(self.value * other.value)
 
     def __iadd__(self, other):
         self.value += other.value
         return self
-
     def __isub__(self, other):
         self.value -= other.value
         return self
-
     def __imul__(self, other):
         self.value *= other.value
         return self
+
+    def __eq__(self, other):
+        return (self.value == other.value)
+    def __ne__(self, other):
+        return (self.value != other.value)
+
+    def __lt__(self, other):
+        return (self.value < other.value)
+    def __le__(self, other):
+        return (self.value <= other.value)
+    def __gt__(self, other):
+        return (self.value > other.value)
+    def __ge__(self, other):
+        return (self.value >= other.value)
 
     def __str__(self):
         return str(self.value)
