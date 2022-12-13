db7777209dec888938f716f6ccafeff48f2de32a
mitsu1119 <hachimitsu1119@gmail.com>
2022-12-13 12:15:14 +0900

    [add]: relation of Integer to int

--- Integer.py
+++ Integer.py
@@ -1,5 +1,5 @@
 
-class Integer:
+class Integer(int):
     def __init__(self, value):
         if isinstance(value, int):
             self.value = value
@@ -7,37 +7,72 @@ class Integer:
             raise TypeError("Integer class must be int value")
 
     def __add__(self, other):
-        return Integer(self.value + other.value)
+        if isinstance(other, self.__class__):
+            return Integer(self.value + other.value)
+        raise TypeError(f"{type(other)}")
     def __sub__(self, other):
-        return Integer(self.value - other.value)
+        if isinstance(other, self.__class__):
+            return Integer(self.value - other.value)
+        raise TypeError(f"{type(other)}")
     def __mul__(self, other):
-        return Integer(self.value * other.value)
+        if isinstance(other, self.__class__):
+            return Integer(self.value * other.value)
+        raise TypeError(f"{type(other)}")
+    def __mod__(self, other):
+        if isinstance(other, self.__class__):
+            return Integer(self.value % other.value)
+        raise TypeError(f"{type(other)}")
 
     def __iadd__(self, other):
-        self.value += other.value
-        return self
+        if isinstance(other, self.__class__):
+            self.value += other.value
+            return self
+        raise TypeError(f"{type(other)}")
     def __isub__(self, other):
-        self.value -= other.value
-        return self
+        if isinstance(other, self.__class__):
+            self.value -= other.value
+            return self
+        raise TypeError(f"{type(other)}")
     def __imul__(self, other):
-        self.value *= other.value
-        return self
+        if isinstance(other, self.__class__):
+            self.value *= other.value
+            return self
+        raise TypeError(f"{type(other)}")
+    def __imod__(self, other):
+        if isinstance(other, self.__class__):
+            self.value %= other.value
+            return self
+        raise TypeError(f"{type(other)}")
 
     def __eq__(self, other):
-        return (self.value == other.value)
+        if isinstance(other, self.__class__):
+            return (self.value == other.value)
+        raise TypeError(f"{type(other)}")
     def __ne__(self, other):
-        return (self.value != other.value)
+        if isinstance(other, self.__class__):
+            return (self.value != other.value)
+        raise TypeError(f"{type(other)}")
 
     def __lt__(self, other):
-        return (self.value < other.value)
+        if isinstance(other, self.__class__):
+            return (self.value < other.value)
+        raise TypeError(f"{type(other)}")
     def __le__(self, other):
-        return (self.value <= other.value)
+        if isinstance(other, self.__class__):
+            return (self.value <= other.value)
+        raise TypeError(f"{type(other)}")
     def __gt__(self, other):
-        return (self.value > other.value)
+        if isinstance(other, self.__class__):
+            return (self.value > other.value)
+        raise TypeError(f"{type(other)}")
     def __ge__(self, other):
-        return (self.value >= other.value)
+        if isinstance(other, self.__class__):
+            return (self.value >= other.value)
+        raise TypeError(f"{type(other)}")
 
     def __str__(self):
+        return str(self.value)
+    def __repr__(self):
         return str(self.value)
 
 ZZ = Integer
