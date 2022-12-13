7559254dcda2e1b00562ce694a326bc0725999d1
mitsu1119 <hachimitsu1119@gmail.com>
2022-12-14 02:00:27 +0900

    [fix] 位数の違う FiniteField 同士で計算できていたバグを修正

--- yoshicrypto/field/FiniteField.py
+++ yoshicrypto/field/FiniteField.py
@@ -17,22 +17,31 @@ class Fp_Integer:
         return self.__parent
 
     # --------------------------------------------------------------------------------------------
+    # Calculatable Type
+    # --------------------------------------------------------------------------------------------
+    def is_calcable(self, other):
+        if isinstance(other, self.__class__):
+            if self.__parent == other.parent():
+                return True
+        return False
+
+    # --------------------------------------------------------------------------------------------
     # Arithmetic Operators
     # --------------------------------------------------------------------------------------------
     def __add__(self, other):
-        if isinstance(other, self.__class__):
+        if self.is_calcable(other):
             return Fp_Integer((self.value + other.value) % ZZ(self.__order).value, self.__order, self.__parent)
         raise TypeError(f"unsupported operand for +: '{str(self.parent())}' and '{str(other.parent())}'")
     def __sub__(self, other):
-        if isinstance(other, self.__class__):
+        if self.is_calcable(other):
             return Fp_Integer((self.value - other.value) % ZZ(self.__order).value, self.__order, self.__parent)
         raise TypeError(f"unsupported operand for -: '{str(self.parent())}' and '{str(other.parent())}'")
     def __mul__(self, other):
-        if isinstance(other, self.__class__):
+        if self.is_calcable(other):
             return Fp_Integer((self.value * other.value) % ZZ(self.__order).value, self.__order, self.__parent)
         raise TypeError(f"unsupported operand for *: '{str(self.parent())}' and '{str(other.parent())}'")
     def __truediv__(self, other):
-        if isinstance(other, self.__class__):
+        if self.is_calcable(other):
             return self * other.inv()
         raise TypeError(f"unsupported operand for /: '{str(self.parent())}' and '{str(other.parent())}'")
     def inv(self):
@@ -42,25 +51,25 @@ class Fp_Integer:
     # Arithmetic and Assignment Operators
     # --------------------------------------------------------------------------------------------
     def __iadd__(self, other):
-        if isinstance(other, self.__class__):
+        if self.is_calcable(other):
             self.value += other.value 
             self.value %= ZZ(self.__order).value
             return self
         raise TypeError(f"unsupported operand for +: '{str(self.parent())}' and '{str(other.parent())}'")
     def __isub__(self, other):
-        if isinstance(other, self.__class__):
+        if self.is_calcable(other):
             self.value -= other.value 
             self.value %= ZZ(self.__order).value
             return self
         raise TypeError(f"unsupported operand for -: '{str(self.parent())}' and '{str(other.parent())}'")
     def __imul__(self, other):
-        if isinstance(other, self.__class__):
+        if self.is_calcable(other):
             self.value *= other.value 
             self.value %= ZZ(self.__order).value
             return self
         raise TypeError(f"unsupported operand for *: '{str(self.parent())}' and '{str(other.parent())}'")
     def __itruediv__(self, other):
-        if isinstance(other, self.__class__):
+        if self.is_calcable(other):
             self.value *= other.inv().value
             self.value %= ZZ(self.__order).value
             return self
@@ -70,27 +79,27 @@ class Fp_Integer:
     # Comparison Operators
     # --------------------------------------------------------------------------------------------
     def __eq__(self, other):
-        if isinstance(other, self.__class__):
+        if self.is_calcable(other):
             return (self.value == other.value)
         raise TypeError(f"unsupported operand for ==: '{str(self.parent())}' and '{str(other.parent())}'")
     def __ne__(self, other):
-        if isinstance(other, self.__class__):
+        if self.is_calcable(other):
             return (self.value != other.value)
         raise TypeError(f"unsupported operand for !=: '{str(self.parent())}' and '{str(other.parent())}'")
     def __lt__(self, other):
-        if isinstance(other, self.__class__):
+        if self.is_calcable(other):
             return (self.value < other.value)
         raise TypeError(f"unsupported operand for <: '{str(self.parent())}' and '{str(other.parent())}'")
     def __le__(self, other):
-        if isinstance(other, self.__class__):
+        if self.is_calcable(other):
             return (self.value <= other.value)
         raise TypeError(f"unsupported operand for <=: '{str(self.parent())}' and '{str(other.parent())}'")
     def __gt__(self, other):
-        if isinstance(other, self.__class__):
+        if self.is_calcable(other):
             return (self.value > other.value)
         raise TypeError(f"unsupported operand for >: '{str(self.parent())}' and '{str(other.parent())}'")
     def __ge__(self, other):
-        if isinstance(other, self.__class__):
+        if self.is_calcable(other):
             return (self.value >= other.value)
         raise TypeError(f"unsupported operand for >=: '{str(self.parent())}' and '{str(other.parent())}'")
 
@@ -136,6 +145,18 @@ class FiniteField:
         if self.__convertable_from(value):
             return Fp_Integer(value, self.__order, self)
         raise ValueError(f"the element of {self.__str__()} must be Integer")
+
+    # --------------------------------------------------------------------------------------------
+    # Comparison Operators
+    # --------------------------------------------------------------------------------------------
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return (self.__order == other.__order)
+        raise TypeError(f"unsupported operand for ==: '{str(self)}' and '{str(other)}'")
+    def __ne__(self, other):
+        if isinstance(other, self.__class__):
+            return (self.__order != other.__order)
+        raise TypeError(f"unsupported operand for !=: '{str(self)}' and '{str(other)}'")
 
     def __str__(self):
         return f"finite field of order {self.__order}"
