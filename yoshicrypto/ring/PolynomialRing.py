4a8a0f59d1f0164b8cdf60fe1fa5e999e632a445
mitsu1119 <hachimitsu1119@gmail.com>
2022-12-28 06:44:01 +0900

    [add] PolynomialRing の変数名の定義

--- yoshicrypto/ring/PolynomialRing.py
+++ yoshicrypto/ring/PolynomialRing.py
@@ -4,10 +4,17 @@ from yoshicrypto.ring.Ring import *
 # Type Factory
 # --------------------------------------------------------------------------------------------
 class PolynomialRing:
-    def __init__(self, base_ring):
+    def __init__(self, base_ring, name):
         if not is_ring(base_ring):
             raise TypeError("base ring of PolynomialRing must be a ring")
         self.__base_ring = base_ring
+
+        s = str(name)
+        if len(s) == 0:
+            raise ValueError("variable name must be nonempty")
+        if not s[0].isalpha():
+            raise ValueError(f"variable name '{s}' is not start with an alphabet")
+        self.__name = s
 
     # --------------------------------------------------------------------------------------------
     # Structure
